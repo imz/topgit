5537670cbfe9a123e8d87962f6c923fb2ca1fcfc
Ivan Zakharyaschev <imz@altlinux.org>
2015-02-19 12:49:53 +0300

    (doc) Present the plan in a more clear way at the beginning.

--- tg-rename.sh
+++ tg-rename.sh
@@ -7,16 +7,42 @@ set -e # exit on any error immediately!
 readonly OLD="$1"
 readonly NEW="$2"
 
+# Thomas Schwinge in <https://groups.google.com/forum/#!topic/git-version-control/AG9AIk3DMBM>:
+#
+# tg rename
+# ~~~~~~~~~
+# 
+# There is no such command yet, but here's a receipe what ``tg rename OLD
+# NEW'' would do:
+# 
+#   - Have a clean state before beginning.
+#   - Assert NEW doesn't already exist.
+#   - git branch -m OLD NEW
+#   - git update-ref refs/top-bases/NEW refs/top-bases/OLD ''
+#   - Clean up the old ref: refs/top-bases/OLD (added by imz@altlinux.org)
+#   - for BRANCH in (all branches that depend on OLD,
+# 		   i.e. reference OLD in .topdeps); do
+#       git checkout BRANCH
+#       sed -i 's%^OLD$%NEW$' .topdeps
+#       git commit -m 'OLD -> NEW' .topdeps
+#     done
+#   - tg summary
+#   - tg update as appropriate
+# 
+# imz@altlinux.org:
+# 
+# This is a poor-man's variant for it, which works only for leaves
+# (i.e., branches that have no dependents, thus omitting the step
+# with a "for"-loop above.)
+
 readonly children=($($tg next "$OLD"))
 
 if [[ "${children[@]}" ]]; then # non-empty
     die "Correct work not implemented for non-leaves, but $OLD has dependents: ${children[@]}."
 fi
 
-# Thomas Schwinge in <https://groups.google.com/forum/#!topic/git-version-control/AG9AIk3DMBM>:
-# 
-# There is no such command yet, but here's a receipe what ``tg rename OLD
-# NEW'' would do:
+# Going through the receipe,
+# leaving the things not implemented as comments.
 # 
 #   - Have a clean state before beginning.
 #   - Assert NEW doesn't already exist.
